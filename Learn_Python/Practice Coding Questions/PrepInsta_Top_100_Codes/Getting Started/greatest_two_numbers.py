8b1fc2355d62daccdcc79fe8a02447c3d25a2dea
Dev-Soumyaranjan <dev.soumya369@gmail.com>
2025-04-27 14:16:04 +0530

    Deleted old folders to implement the new, updated folder structure for the learning journey. The new structure includes Core Programming, Data Engineering, and other categories that will be populated with content as I progress.

--- Learn_Python/Practice Coding Questions/PrepInsta_Top_100_Codes/Getting Started/greatest_two_numbers.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    # Taking user input by calling take_utils_int() defined in utils.py
-    firstNum = int(input('Enter first number: '))
-    secondNum = int(input('Enter second number: '))
-
-    # I have used ternary operator in python using if-else. The First print statement will be executed if the
-    # condition is true, otherwise the second print will be executed
-    print(firstNum, " is greater than ", secondNum) if firstNum > secondNum else print(secondNum, " is greater than ",
-                                                                                       firstNum)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Data-Engineering-Ascent
-This is my own personal repository for learning data engineering, with a wide range of notes, code samples, and practice materials.Let's explore the world of data engineering together🚀
